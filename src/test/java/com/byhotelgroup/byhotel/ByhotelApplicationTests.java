567aa5d7ccb5ffcb953a2af0370c04bce1eff2b7
Kutay <osmkutay97@gmail.com>
2024-08-10 14:09:54 +0300

    First commit

---
+++ src/test/java/com/byhotelgroup/byhotel/ByhotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byhotelgroup.byhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByhotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
